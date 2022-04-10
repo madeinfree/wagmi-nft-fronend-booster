42572380ccc810de2223ef51da87b91ec9b70228
madeinfree <sal95610@gmail.com>
2022-04-10 23:43:09 +0800

    fix: fixed user reject(cancel) metamask tx
    * the wagmi replace 4001 to "User rejected request" so we only use message to check user action.

--- app/utils/errorFormat.js
+++ app/utils/errorFormat.js
@@ -1,5 +1,13 @@
+const ERROR_CANCEL = 'User rejected request'
+const ERROR_REVERT = -32603
+
 export const mintErrorFormat = (error) => {
-  if (error.error.code === -32603) {
+  if (error?.error?.code === ERROR_REVERT) {
     return `${error.error.message} - 預期此交易可能會執行失敗!`
   }
+  if (error.message === ERROR_CANCEL) {
+    return `${error.message} - 交易取消!`
+  }
+
+  return `非預期錯誤`
 }
