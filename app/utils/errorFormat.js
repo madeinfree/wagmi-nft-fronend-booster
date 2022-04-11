6290d1333ceb5fb5d368578ad31864507d883b1a
madeinfree <sal95610@gmail.com>
2022-04-11 17:22:38 +0800

    docs: add comment for formats function
    * add comment for formats function

--- app/utils/errorFormat.js
+++ app/utils/errorFormat.js
@@ -1,6 +1,11 @@
 const ERROR_CANCEL = 'User rejected request'
 const ERROR_REVERT = -32603
 
+/**
+ * @dev Formats contract response error message
+ * @param {*} error
+ * @returns string
+ */
 export const mintErrorFormat = (error) => {
   if (error?.error?.code === ERROR_REVERT) {
     return `${error.error.message} - 預期此交易可能會執行失敗!`
